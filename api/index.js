1fb2d9b56671d70b572e4e636f9a5ba36f7b0d74
himansh-01 <himanshprajapati2004@gmail.com>
2025-01-24 23:59:49 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.use(cors({credentials: true,origin:"http://localhost:3000"}));
 app.use(express.json());
